37bdba068ec1ae970fe637d21a89d0b1ba21ef79
everl0ve <wangzifan_wzf@hotmail.com>
2018-09-18 11:11:55 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import logging
+import os
+
+import cv2
+import numpy as np
+import tensorflow as tf
+from vgg import vgg_16
+
+flags = tf.app.flags
+flags.DEFINE_string('data_dir', '', 'Root directory to raw pet dataset.')
+flags.DEFINE_string('output_dir', '', 'Path to directory to output TFRecords.')
+
+FLAGS = flags.FLAGS
+
+def read_images_names(root, train=True):
+    txt_fname = os.path.join(root, 'ImageSets/Segmentation/', 'train.txt' if train else 'val.txt')
+
+    with open(txt_fname, 'r') as f:
+        images = f.read().split()
+
+    data = []
+    label = []
+    for fname in images:
+        data.append('%s/JPEGImages/%s.jpg' % (root, fname))
+        label.append('%s/SegmentationClass/%s.png' % (root, fname))
+    return zip(data, label)
+
+
+
+def main(_):
+    logging.info('Prepare dataset file names')
+
+    train_output_path = os.path.join(FLAGS.output_dir, 'fcn_train.record')
+    val_output_path = os.path.join(FLAGS.output_dir, 'fcn_val.record')
+
+    train_files = read_images_names(FLAGS.data_dir, True)
+    val_files = read_images_names(FLAGS.data_dir, False)
+    print(list(train_files))
+
+if __name__ == '__main__':
+    tf.app.run()   
